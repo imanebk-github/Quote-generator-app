7d9d0a095d985d99eaa6dcf4474237f33751d9b9
IMANE <imane.elbakir@gmail.com>
2023-09-22 19:33:44 +0100

    added generating button event and sound

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
+const synth = window.speechSynthesis;
+
 const quoteText = document.querySelector('.quote');
 const quoteBtn = document.querySelector('.new-quote');
+const author = document.querySelector('.name');
+const soundbt = document.querySelector('.sound');
+const copydbt = document.querySelector('.copy');
+const twitterbt = document.querySelector('.twitter');
 
-quoteBtn.addEventListener('click', () =>)
+
+// Random quote Function
+const randomQuote = () => {
+    quoteBtn.classList.add('loading');
+    quoteBtn.innerHTML = 'loading quote...';
+    // fetching random quote/data from API and parsing it into javascript object
+    fetch('https://api.quotable.io/random').then(res => res.json()).then(result =>{
+        console.log(result);
+        quoteText.innerText = result.content;
+        author.innerText = result.author
+        quoteBtn.innerText = 'New Quote';
+        quoteBtn.classList.remove('loading');
+    })
+}
+
+soundbt.addEventListener('click', () => {
+    // Web Speech API
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText}`);
+    utterance.lang = "en-UK";
+    let voices = window.speechSynthesis.getVoices();
+
+    let maleVoice = null;
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name.includes("Male")) {
+            maleVoice = voices[i];
+            break;
+        }
+    }
+    if (maleVoice) {
+        utterance.voice = maleVoice;
+        utterance.voiceURI = maleVoice.voiceURI;
+    }
+    speechSynthesis.speak(utterance);
+})
+quoteBtn.addEventListener('click', randomQuote);
